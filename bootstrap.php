bb1c463a3c29567bdbf8a33a6d664785925e77d4
Fred Emmott <github@fredemmott.co.uk>
2015-02-22 02:26:53 +0000

    Move bootstrap.php as its useful for others

--- bootstrap.php
+++ bootstrap.php
@@ -9,5 +9,5 @@
  *
  */
 
-require_once(__DIR__.'/../src/DefinitionToken.php');
-require_once(__DIR__.'/../src/FileParser.php');
+require_once(__DIR__.'/src/DefinitionToken.php');
+require_once(__DIR__.'/src/FileParser.php');
