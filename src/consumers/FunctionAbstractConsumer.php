a815653ad5b15c458247df5d1e192f3abcae69cc
Fred Emmott <fe@fb.com>
2015-08-11 15:45:26 -0700

    Split typehint consumer out

--- src/consumers/FunctionAbstractConsumer.php
+++ src/consumers/FunctionAbstractConsumer.php
@@ -65,7 +65,7 @@ abstract class FunctionAbstractConsumer<T as ScannedFunctionAbstract>
     if ($t === ':') {
       $tq->shift();
       $this->consumeWhitespace();
-      $builder->setReturnType($this->consumeType());
+      $builder->setReturnType((new TypehintConsumer($this->tq))->getTypehint());
     }
     return $builder;
   }
@@ -148,115 +148,9 @@ abstract class FunctionAbstractConsumer<T as ScannedFunctionAbstract>
         $tq->getLine(),
       );
       $tq->unshift($t, $ttype);
-      $param_type = $this->consumeType();
+      $param_type = (new TypehintConsumer($this->tq))->getTypehint();
     }
     return $params;
-  }
-
-  private function consumeType(): ScannedTypehint {
-    $type = null;
-    $generics = Vector { };
-
-    $nesting = 0;
-    while ($this->tq->haveTokens()) {
-      list($t, $ttype) = $this->tq->shift();
-
-      if ($ttype === T_WHITESPACE) {
-        if ($nesting === 0) {
-          break;
-        }
-        continue;
-      }
-
-      // Handle functions
-      if ($t === '(') {
-        $this->consumeWhitespace();
-        list($t, $ttype) = $this->tq->peek();
-        if ($ttype === T_FUNCTION) {
-          ++$nesting;
-          continue;
-        }
-
-        $type = 'tuple';
-        while ($this->tq->haveTokens()) {
-          $this->consumeWhitespace();
-          $generics[] = $this->consumeType();
-          $this->consumeWhitespace();
-
-          list($t, $_) = $this->tq->shift();
-          if ($t === ')') {
-            break;
-          }
-          invariant(
-            $t === ',',
-            'expected ) or , after tuple member at line %d',
-            $this->tq->getLine(),
-          );
-        }
-        break;
-      }
-      if ($t === ')') {
-        --$nesting;
-        if ($nesting === 0) {
-          break;
-        }
-        continue;
-      }
-
-      if (
-        $ttype !== T_STRING
-        && $ttype !== T_NS_SEPARATOR
-        && $ttype !== T_CALLABLE
-        && $ttype !== T_ARRAY
-      ) {
-        continue;
-      }
-
-      $type = $t;
-      // Handle \foo
-      if ($ttype === T_NS_SEPARATOR) {
-        list($t, $_) = $this->tq->shift();
-        $type .= $t;
-      }
-
-      // Handle \foo\bar and foo\bar
-      while ($this->tq->haveTokens()) {
-        list($_, $ttype) = $this->tq->peek();
-        if ($ttype !== T_NS_SEPARATOR) {
-          break;
-        }
-        $this->tq->shift();
-        $type .= "\\";
-        list($t, $_) = $this->tq->shift();
-        $type .= $t;
-      }
-
-      // consume generics and recurse
-      $this->consumeWhitespace();
-      list($t, $ttype) = $this->tq->peek();
-      if ($ttype === T_TYPELIST_LT) {
-        $this->tq->shift();
-        while ($this->tq->haveTokens()) {
-          $this->consumeWhitespace();
-          $generics[] = $this->consumeType();
-          $this->consumeWhitespace();
-
-          list($t, $ttype) = $this->tq->shift();
-          if ($ttype === T_TYPELIST_GT) {
-            break;
-          }
-          invariant(
-            $t === ',',
-            'expected > or , after generic type at line %d',
-            $this->tq->getLine(),
-          );
-        }
-        break;
-      }
-      break;
-    }
-    invariant($type !== null, 'expected a type at line %d', $this->tq->getLine());
-    return new ScannedTypehint($type, $generics);
   }
 
   private function consumeGenerics(): \ConstVector<ScannedGeneric> {
---
+++ src/consumers/TypehintConsumer.php
@@ -0,0 +1,124 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace Facebook\DefinitionFinder;
+
+final class TypehintConsumer extends Consumer {
+  public function getTypehint(): ScannedTypehint {
+    return $this->consumeType();
+  }
+
+  private function consumeType(): ScannedTypehint {
+    $type = null;
+    $generics = Vector { };
+
+    $nesting = 0;
+    while ($this->tq->haveTokens()) {
+      list($t, $ttype) = $this->tq->shift();
+
+      if ($ttype === T_WHITESPACE) {
+        if ($nesting === 0) {
+          break;
+        }
+        continue;
+      }
+
+      // Handle functions
+      if ($t === '(') {
+        $this->consumeWhitespace();
+        list($t, $ttype) = $this->tq->peek();
+        if ($ttype === T_FUNCTION) {
+          ++$nesting;
+          continue;
+        }
+
+        $type = 'tuple';
+        while ($this->tq->haveTokens()) {
+          $this->consumeWhitespace();
+          $generics[] = $this->consumeType();
+          $this->consumeWhitespace();
+
+          list($t, $_) = $this->tq->shift();
+          if ($t === ')') {
+            break;
+          }
+          invariant(
+            $t === ',',
+            'expected ) or , after tuple member at line %d',
+            $this->tq->getLine(),
+          );
+        }
+        break;
+      }
+      if ($t === ')') {
+        --$nesting;
+        if ($nesting === 0) {
+          break;
+        }
+        continue;
+      }
+
+      if (
+        $ttype !== T_STRING
+        && $ttype !== T_NS_SEPARATOR
+        && $ttype !== T_CALLABLE
+        && $ttype !== T_ARRAY
+      ) {
+        continue;
+      }
+
+      $type = $t;
+      // Handle \foo
+      if ($ttype === T_NS_SEPARATOR) {
+        list($t, $_) = $this->tq->shift();
+        $type .= $t;
+      }
+
+      // Handle \foo\bar and foo\bar
+      while ($this->tq->haveTokens()) {
+        list($_, $ttype) = $this->tq->peek();
+        if ($ttype !== T_NS_SEPARATOR) {
+          break;
+        }
+        $this->tq->shift();
+        $type .= "\\";
+        list($t, $_) = $this->tq->shift();
+        $type .= $t;
+      }
+
+      // consume generics and recurse
+      $this->consumeWhitespace();
+      list($t, $ttype) = $this->tq->peek();
+      if ($ttype === T_TYPELIST_LT) {
+        $this->tq->shift();
+        while ($this->tq->haveTokens()) {
+          $this->consumeWhitespace();
+          $generics[] = $this->consumeType();
+          $this->consumeWhitespace();
+
+          list($t, $ttype) = $this->tq->shift();
+          if ($ttype === T_TYPELIST_GT) {
+            break;
+          }
+          invariant(
+            $t === ',',
+            'expected > or , after generic type at line %d',
+            $this->tq->getLine(),
+          );
+        }
+        break;
+      }
+      break;
+    }
+    invariant($type !== null, 'expected a type at line %d', $this->tq->getLine());
+    return new ScannedTypehint($type, $generics);
+  }
+}
