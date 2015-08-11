ed8aa2d4d968b4757c80f3a7d75fbb19dd4add9c
Fred Emmott <fe@fb.com>
2015-08-11 10:42:17 -0700

    add systemlib test

--- tests/SelfTest.php
+++ tests/SelfTest.php
@@ -31,4 +31,74 @@ class SelfTest extends \PHPUnit_Framework_TestCase {
     $parser = FileParser::FromFile($filename);
     $this->assertNotNull($parser);
   }
+
+  public function testSystemLib(): void {
+    $systemlib = '/tmp/systemlib.php';
+    file_put_contents($systemlib, $this->getSection('systemlib'));
+    $parser = FileParser::FromFile($systemlib);
+  }
+
+  public function testExtensions(): void {
+    $this->markTestIncomplete('Fix systemlib first ^_^');
+  }
+
+  <<__Memoize>>
+  private function getReadelf(): string {
+    $readelf = trim(shell_exec('which readelf'));
+    if (!is_executable($readelf)) {
+      $this->markTestSkipped('could not find readelf');
+    }
+    return $readelf;
+  }
+
+  private function getSection(
+    string $section_name,
+  ): string {
+    invariant(
+      $this->getSectionList()->contains($section_name),
+      '%s is not a section name',
+      $section_name,
+    );
+
+    $cmd = Vector {
+      $this->getReadelf(),
+      '--hex-dump', // --string-dump does some escaping :(
+      $section_name,
+      '--wide',
+      PHP_BINARY,
+    };
+    $raw = shell_exec(implode(' ', $cmd->map($x ==>escapeshellarg($x))));
+    $bytes = (new Vector(explode("\n", $raw)))
+      // 0xADDR deadbeef deadbeef deadbeef deadbeef <?hh foo ba
+      ->filter($line ==> strpos($line, '  0x') === 0)
+      // deadbeefdeadbeefdeadbeefdeadbeef
+      ->map($line ==> implode('', array_slice(explode(' ', trim($line)), 1, 4)))
+      ->map($hex ==> hex2bin($hex));
+    $bytes = implode('', $bytes);
+
+    // array_map() and a few other functions in SystemLib are implemented in
+    // HH ASsembly instead of Hack/PHP. These need to be ignored, and are always
+    // at the end, after this marker.
+    //
+    // This approach is used in HPHP::systemlib_split() when HHVM laods
+    // systemlib.
+    $bytes = explode("\n<?hhas\n", $bytes)[0];
+
+    return $bytes;
+  }
+
+  <<__Memoize>>
+  private function getSectionList(): Set<string> {
+    $parts = Vector {
+      $this->getReadelf(),
+      '--section-headers',
+      PHP_BINARY
+    };
+    $sections = shell_exec(implode(' ', $parts->map($x ==> escapeshellarg($x))));
+    $sections = (new Set(explode("\n", $sections)))
+      ->filter($line ==> strpos($line, 'PROGBITS') !== false)
+      ->map($line ==> preg_split('/\s+/', $line)[2])
+      ->filter($name ==> preg_match('/^(ext\.|systemlib$)/', $name) !== 0);
+    return $sections;
+  }
 }
