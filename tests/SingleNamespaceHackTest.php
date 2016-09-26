78b2e7b77a71aee6f8fe549ec255afa630d8442c
Fred Emmott <fred@fredemmott.co.uk>
2016-09-25 22:14:14 -0700

    Test that namespaced references are properly normalized
    fixes #27

--- tests/SingleNamespaceHackTest.php
+++ tests/SingleNamespaceHackTest.php
@@ -9,12 +9,36 @@
  *
  */
 
-class SingleNamespaceHackTest extends AbstractHackTest {
+namespace Facebook\DefinitionFinder\Test;
+
+use Facebook\DefinitionFinder\FileParser;
+
+final class SingleNamespaceHackTest extends \AbstractHackTest {
   protected function getFilename(): string {
     return 'single_namespace_hack.php';
   }
 
   protected function getPrefix(): string {
     return 'SingleNamespace\\';
+  }
+
+  public function testConsistentNames(): void {
+    $data =
+      "<?hh\n".
+      "class Herp extends Foo\Bar {}\n".
+      "class Derp extends \Foo\Bar {}\n";
+
+    $parser = FileParser::FromData($data);
+    $herp = $parser->getClass('Herp');
+    $derp = $parser->getClass('Derp');
+
+    $this->assertSame(
+      'Foo\Bar',
+      $herp->getParentClassName(),
+    );
+    $this->assertSame(
+      $herp->getParentClassName(),
+      $derp->getParentClassName(),
+    );
   }
 }
