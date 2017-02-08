kilahm/definitions-finder

+getConstraintRelationship() -getConstraint() +getConstraintTypeName()

tests/GenericsTest.php
@@ -3,6 +3,7 @@
 namespace FredEmmott\DefinitionFinder\Test;
 
 use FredEmmott\DefinitionFinder\FileParser;
+use FredEmmott\DefinitionFinder\RelationshipToken;
 
 class GenericsTest extends \PHPUnit_Framework_TestCase {
   public function testClassHasGenerics(): void {
@@ -17,7 +18,7 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { null, null },
-      $class->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
 
@@ -33,7 +34,7 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { null, null },
-      $function->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $function->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
 
@@ -44,9 +45,12 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { 'Bar', 'Baz' },
-      $class->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
-    $this->markTestIncomplete('check for subtype or supertype');
+    $this->assertEquals(
+      Vector { RelationshipToken::SUBTYPE, RelationshipToken::SUPERTYPE },
+      $class->getGenericTypes()->map($x ==> $x->getConstraintRelationship()),
+    );
   }
 
   public function testVariance(): void {

hhvm/definition-finder

tests/GenericsTest.php
@@ -12,6 +12,7 @@
 namespace Facebook\DefinitionFinder\Test;
 
 use Facebook\DefinitionFinder\FileParser;
+use Facebook\DefinitionFinder\RelationshipToken;
 
 class GenericsTest extends \PHPUnit_Framework_TestCase {
   public function testClassHasGenerics(): void {
@@ -26,7 +27,7 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { null, null },
-      $class->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
 
@@ -42,7 +43,7 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { null, null },
-      $function->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $function->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
 
@@ -53,9 +54,12 @@ class GenericsTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector { 'Bar', 'Baz' },
-      $class->getGenericTypes()->map($x ==> $x->getConstraint()),
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
-    $this->markTestIncomplete('check for subtype or supertype');
+    $this->assertEquals(
+      Vector { RelationshipToken::SUBTYPE, RelationshipToken::SUPERTYPE },
+      $class->getGenericTypes()->map($x ==> $x->getConstraintRelationship()),
+    );
   }
 
   public function testVariance(): void {