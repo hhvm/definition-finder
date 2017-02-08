kilahm/definitions-finder

Capitalization of static functions

tests/AbstractHackTest.php
@@ -7,7 +7,7 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
   abstract protected function getPrefix(): string;
 
   protected function setUp(): void {
-    $this->parser = \FredEmmott\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \FredEmmott\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/'.$this->getFilename(),
     );
   }

tests/AbstractPHPTest.php
@@ -7,7 +7,7 @@ abstract class AbstractPHPTest extends PHPUnit_Framework_TestCase {
   abstract protected function getPrefix(): string;
 
   protected function setUp(): void {
-    $this->parser = \FredEmmott\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \FredEmmott\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/'.$this->getFilename(),
     );
   }

tests/MultiNamespacePHPTest.php
@@ -4,7 +4,7 @@ final class MultiNamespacePHPTest extends PHPUnit_Framework_TestCase {
   private ?FredEmmott\DefinitionFinder\FileParser $parser;
 
   protected function setUp(): void {
-    $this->parser = \FredEmmott\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \FredEmmott\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/multi_namespace_php.php',
     );
   }

hhvm/definition-finder

tests/AbstractHackTest.php
@@ -16,7 +16,7 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
   abstract protected function getPrefix(): string;
 
   protected function setUp(): void {
-    $this->parser = \Facebook\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \Facebook\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/'.$this->getFilename(),
     );
   }

tests/AbstractPHPTest.php
@@ -16,7 +16,7 @@ abstract class AbstractPHPTest extends PHPUnit_Framework_TestCase {
   abstract protected function getPrefix(): string;
 
   protected function setUp(): void {
-    $this->parser = \Facebook\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \Facebook\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/'.$this->getFilename(),
     );
   }

tests/MultiNamespacePHPTest.php
@@ -13,7 +13,7 @@ final class MultiNamespacePHPTest extends PHPUnit_Framework_TestCase {
   private ?Facebook\DefinitionFinder\FileParser $parser;
 
   protected function setUp(): void {
-    $this->parser = \Facebook\DefinitionFinder\FileParser::fromFile(
+    $this->parser = \Facebook\DefinitionFinder\FileParser::FromFile(
       __DIR__.'/data/multi_namespace_php.php',
     );
   }