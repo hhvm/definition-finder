kilahm/definitions-finder

Interpret use statements (#14)
* Interpret use statements

Use the aliases defined with "use Foo\Bar as Baz" to reference the FQN
of parameter, return, and generic type hints.

* Style

tests/AbstractHackTest.php
@@ -1,7 +1,9 @@
 <?hh // strict
 
+use FredEmmott\DefinitionFinder\ScannedClass;
 use FredEmmott\DefinitionFinder\ScannedFunction;
-use FredEmmott\DefinitionFinder\ScannedTypehint;
+use FredEmmott\DefinitionFinder\ScannedFunctionAbstract;
+use FredEmmott\DefinitionFinder\ScannedMethod;
 
 abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
   private ?FredEmmott\DefinitionFinder\FileParser $parser;
@@ -20,6 +22,7 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
       Vector {
         $this->getPrefix().'SimpleClass',
         $this->getPrefix().'GenericClass',
+        $this->getPrefix().'GenericAliasedConstraintClass',
         $this->getPrefix().'AbstractFinalClass',
         $this->getPrefix().'AbstractClass',
         $this->getPrefix().'xhp_foo',
@@ -71,6 +74,11 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
         $this->getPrefix().'returns_int',
         $this->getPrefix().'returns_generic',
         $this->getPrefix().'returns_nested_generic',
+        $this->getPrefix().'aliased',
+        $this->getPrefix().'aliased_with_namespace',
+        $this->getPrefix().'aliased_with_nested_namespace',
+        $this->getPrefix().'aliased_namespace',
+        $this->getPrefix().'aliased_no_as',
       },
       $this->parser?->getFunctionNames(),
     );
@@ -101,6 +109,21 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector {null, null},
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
+    );
+
+    $class = $this->parser?->getClass(
+      $this->getPrefix().'GenericAliasedConstraintClass'
+    );
+    assert($class !== null);
+
+    $this->assertEquals(
+      Vector {'T'},
+      $class->getGenericTypes()->map($x ==> $x->getName()),
+    );
+
+    $this->assertEquals(
+      Vector {'Foo'},
       $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
@@ -157,9 +180,61 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
     $this->assertEmpty($sub_sub_type?->getGenericTypes());
   }
 
+  public function testAliasedTypehints(): void {
+    $data = Map {
+      'Foo' => $this->getFunction('aliased'),
+        'SingleNamespace\Foo' => $this->getFunction(
+          'aliased_with_namespace'
+        ),
+      'Namespaces\AreNested\Now\Foo' => $this->getFunction(
+        'aliased_with_nested_namespace'
+      ),
+      'Namespaces\AreNested\Now\Foo' => $this->getFunction(
+        'aliased_namespace'
+      ),
+      'Namespaces\AreNested\Now\Bar' => $this->getFunction(
+        'aliased_no_as'
+      ),
+      'Namespaces\AreNested\Now\Bar' => $this->getClassMethod(
+        'SimpleClass',
+        'aliasInClassScope'
+      ),
+    };
+    foreach($data as $typeName => $fun) {
+      $returnType = $fun->getReturnType();
+      $paramType = $fun->getParameters()->get(0)?->getTypehint();
+      $this->assertSame($typeName, $returnType?->getTypeName());
+      $this->assertSame($typeName, $paramType?->getTypeName());
+    }
+  }
+
   private function getFunction(string $name): ScannedFunction {
     $func = $this->parser?->getFunction($this->getPrefix().$name);
     invariant($func !== null, 'Could not find function %s', $name);
     return $func;
+  }
+
+  private function getClass(string $name): ScannedClass {
+    $class = $this->parser?->getClass($this->getPrefix().$name);
+    invariant($class !== null, 'Could not find class %s', $name);
+    return $class;
+  }
+
+  private function getClassMethod(
+    string $className,
+    string $methodName
+  ): ScannedMethod {
+    $method = $this
+      ->getClass($className)
+      ->getMethods()
+      ->filter($m ==> $m->getName() === $methodName)
+      ->get(0);
+    invariant(
+      $method !== null,
+      'Could not find method %s in class %s',
+      $methodName,
+      $className
+    );
+    return $method;
   }
 }

hhvm/definition-finder

tests/AbstractHackTest.php
@@ -9,8 +9,10 @@
  *
  */
 
+use Facebook\DefinitionFinder\ScannedClass;
 use Facebook\DefinitionFinder\ScannedFunction;
-use Facebook\DefinitionFinder\ScannedTypehint;
+use Facebook\DefinitionFinder\ScannedFunctionAbstract;
+use Facebook\DefinitionFinder\ScannedMethod;
 
 abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
   private ?Facebook\DefinitionFinder\FileParser $parser;
@@ -29,6 +31,7 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
       Vector {
         $this->getPrefix().'SimpleClass',
         $this->getPrefix().'GenericClass',
+        $this->getPrefix().'GenericAliasedConstraintClass',
         $this->getPrefix().'AbstractFinalClass',
         $this->getPrefix().'AbstractClass',
         $this->getPrefix().'xhp_foo',
@@ -80,6 +83,11 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
         $this->getPrefix().'returns_int',
         $this->getPrefix().'returns_generic',
         $this->getPrefix().'returns_nested_generic',
+        $this->getPrefix().'aliased',
+        $this->getPrefix().'aliased_with_namespace',
+        $this->getPrefix().'aliased_with_nested_namespace',
+        $this->getPrefix().'aliased_namespace',
+        $this->getPrefix().'aliased_no_as',
       },
       $this->parser?->getFunctionNames(),
     );
@@ -110,6 +118,21 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals(
       Vector {null, null},
+      $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
+    );
+
+    $class = $this->parser?->getClass(
+      $this->getPrefix().'GenericAliasedConstraintClass'
+    );
+    assert($class !== null);
+
+    $this->assertEquals(
+      Vector {'T'},
+      $class->getGenericTypes()->map($x ==> $x->getName()),
+    );
+
+    $this->assertEquals(
+      Vector {'Foo'},
       $class->getGenericTypes()->map($x ==> $x->getConstraintTypeName()),
     );
   }
@@ -166,9 +189,61 @@ abstract class AbstractHackTest extends PHPUnit_Framework_TestCase {
     $this->assertEmpty($sub_sub_type?->getGenericTypes());
   }
 
+  public function testAliasedTypehints(): void {
+    $data = Map {
+      'Foo' => $this->getFunction('aliased'),
+        'SingleNamespace\Foo' => $this->getFunction(
+          'aliased_with_namespace'
+        ),
+      'Namespaces\AreNested\Now\Foo' => $this->getFunction(
+        'aliased_with_nested_namespace'
+      ),
+      'Namespaces\AreNested\Now\Foo' => $this->getFunction(
+        'aliased_namespace'
+      ),
+      'Namespaces\AreNested\Now\Bar' => $this->getFunction(
+        'aliased_no_as'
+      ),
+      'Namespaces\AreNested\Now\Bar' => $this->getClassMethod(
+        'SimpleClass',
+        'aliasInClassScope'
+      ),
+    };
+    foreach($data as $typeName => $fun) {
+      $returnType = $fun->getReturnType();
+      $paramType = $fun->getParameters()->get(0)?->getTypehint();
+      $this->assertSame($typeName, $returnType?->getTypeName());
+      $this->assertSame($typeName, $paramType?->getTypeName());
+    }
+  }
+
   private function getFunction(string $name): ScannedFunction {
     $func = $this->parser?->getFunction($this->getPrefix().$name);
     invariant($func !== null, 'Could not find function %s', $name);
     return $func;
+  }
+
+  private function getClass(string $name): ScannedClass {
+    $class = $this->parser?->getClass($this->getPrefix().$name);
+    invariant($class !== null, 'Could not find class %s', $name);
+    return $class;
+  }
+
+  private function getClassMethod(
+    string $className,
+    string $methodName
+  ): ScannedMethod {
+    $method = $this
+      ->getClass($className)
+      ->getMethods()
+      ->filter($m ==> $m->getName() === $methodName)
+      ->get(0);
+    invariant(
+      $method !== null,
+      'Could not find method %s in class %s',
+      $methodName,
+      $className
+    );
+    return $method;
   }
 }