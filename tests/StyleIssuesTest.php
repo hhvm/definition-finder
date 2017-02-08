kilahm/definitions-finder

Add already-passing test for whitespace around ':' (return types)

tests/StyleIssuesTest.php
@@ -3,6 +3,7 @@
 namespace FredEmmott\DefinitionFinder\Test;
 
 use FredEmmott\DefinitionFinder\FileParser;
+use FredEmmott\DefinitionFinder\ScannedTypehint;
 
 class StyleIssuesTest extends \PHPUnit_Framework_TestCase {
   public function testFunctionWithWhitespaceBeforeParamsList(): void {
@@ -12,6 +13,16 @@ class StyleIssuesTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(
       Vector { '$bar' },
       $fun->getParameters()->map($x ==> $x->getName()),
+    );
+  }
+
+  public function testFunctionWithWhitespaceBeforeReturnType(): void {
+    $data = '<?hh function foo() : void {}';
+    $parser = FileParser::FromData($data);
+    $fun = $parser->getFunction('foo');
+    $this->assertEquals(
+      new ScannedTypehint('void', Vector { }),
+      $fun->getReturnType(),
     );
   }
 }

hhvm/definition-finder

tests/StyleIssuesTest.php
@@ -12,6 +12,7 @@
 namespace Facebook\DefinitionFinder\Test;
 
 use Facebook\DefinitionFinder\FileParser;
+use Facebook\DefinitionFinder\ScannedTypehint;
 
 class StyleIssuesTest extends \PHPUnit_Framework_TestCase {
   public function testFunctionWithWhitespaceBeforeParamsList(): void {
@@ -21,6 +22,16 @@ class StyleIssuesTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(
       Vector { '$bar' },
       $fun->getParameters()->map($x ==> $x->getName()),
+    );
+  }
+
+  public function testFunctionWithWhitespaceBeforeReturnType(): void {
+    $data = '<?hh function foo() : void {}';
+    $parser = FileParser::FromData($data);
+    $fun = $parser->getFunction('foo');
+    $this->assertEquals(
+      new ScannedTypehint('void', Vector { }),
+      $fun->getReturnType(),
     );
   }
 }