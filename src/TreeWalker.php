kilahm/definitions-finder

Implement TreeWalker
Fixes #3

Finds all the definitions in a tree. Optionally takes a filter function.

src/TreeWalker.php
@@ -1,0 +1,82 @@
+<?hh // strict
+
+namespace FredEmmott\DefinitionFinder;
+
+class TreeWalker implements TreeDefinitions {
+  private Map<string, Set<string>> $classes = Map { };
+  private Map<string, Set<string>> $interfaces = Map { };
+  private Map<string, Set<string>> $traits = Map { };
+  private Map<string, Set<string>> $enums = Map { };
+  private Map<string, Set<string>> $types = Map { };
+  private Map<string, Set<string>> $newtypes = Map { };
+  private Map<string, Set<string>> $functions = Map { };
+  private Map<string, Set<string>> $constants = Map { };
+
+  /**
+   * Create a TreeWalker
+   *
+   * @param $root - the root directory
+   * @param $path_filter_func - called for every file name before it is parsed.
+   *   If it returns false, the file is not parsed. Handy if there's something
+   *   in vendor/ or your tests that does not parse.
+   */
+  public function __construct(
+    string $root,
+    ?(function(string): bool) $path_filter_func = null,
+  ) {
+    $dit = new \RecursiveDirectoryIterator($root) /* HH_FIXME[2049] no HHI */;
+    $rit = new \RecursiveIteratorIterator($dit) /* HH_FIXME[2049] no HHI */;
+    $files = Vector { };
+    foreach ($rit as $path => $info) {
+      if ($info->isDir() || $info->isLink() || !$info->isReadable()) {
+        continue;
+      }
+
+      $ext = $info->getExtension();
+      if ($ext !== 'php' && $ext !== 'hh') {
+        continue;
+      }
+
+      if ($path_filter_func !== null) {
+        if (!$path_filter_func($path)) {
+          continue;
+        }
+      }
+
+      $files[] = $path;
+    }
+    foreach ($files as $file) {
+      $fp = FileParser::FromFile($file);
+      self::addDefs($this->classes, $file, $fp->getClasses());
+      self::addDefs($this->interfaces, $file, $fp->getInterfaces());
+      self::addDefs($this->traits, $file, $fp->getTraits());
+      self::addDefs($this->enums, $file, $fp->getEnums());
+      self::addDefs($this->types, $file, $fp->getTypes());
+      self::addDefs($this->newtypes, $file, $fp->getNewtypes());
+      self::addDefs($this->functions, $file, $fp->getFunctions());
+      self::addDefs($this->constants, $file, $fp->getConstants());
+    }
+  }
+
+  private static function addDefs(
+    Map<string, Set<string>> $container,
+    string $file,
+    \ConstVector<string> $defs,
+  ): void {
+    foreach ($defs as $def) {
+      if (!$container->containsKey($def)) {
+        $container[$def] = Set { };
+      }
+      $container[$def]->add($file);
+    }
+  }
+
+  public function getClasses(): \ConstMap<string, Set<string>> { return $this->classes; }
+  public function getInterfaces(): \ConstMap<string, Set<string>> { return $this->interfaces; }
+  public function getTraits(): \ConstMap<string, Set<string>> { return $this->traits; }
+  public function getEnums(): \ConstMap<string, Set<string>> { return $this->enums; }
+  public function getTypes(): \ConstMap<string, Set<string>> { return $this->types; }
+  public function getNewtypes(): \ConstMap<string, Set<string>> { return $this->newtypes; }
+  public function getFunctions(): \ConstMap<string, Set<string>> { return $this->functions; }
+  public function getConstants(): \ConstMap<string, Set<string>> { return $this->constants; }
+}

tests/TreeWalkerTest.php
@@ -1,0 +1,64 @@
+<?hh // strict
+
+use \FredEmmott\DefinitionFinder\TreeWalker;
+
+final class TreeWalkerTest extends PHPUnit_Framework_TestCase {
+  private function createWalker(
+    ?(function(string):bool) $filter
+  ): TreeWalker {
+    return new TreeWalker(realpath(__DIR__.'/../'), $filter);
+  }
+
+  public function testHasThisLibraryDefinitions(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains(
+      'FredEmmott\DefinitionFinder\TreeWalker',
+      $w->getClasses()->keys(),
+    );
+    $this->assertContains(
+      'FredEmmott\DefinitionFinder\DefinitionToken',
+      $w->getEnums()->keys(),
+    );
+    $this->assertContains(
+      'FredEmmott\DefinitionFinder\TreeDefinitions',
+      $w->getInterfaces()->keys(),
+    );
+
+    $this->assertContains(
+      realpath(__DIR__.'/../src/TreeWalker.php'),
+      $w->getClasses()['FredEmmott\DefinitionFinder\TreeWalker'],
+    );
+  }
+
+  public function testEachDefinitionKind(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertContains('SimpleInterface', $w->getInterfaces()->keys());
+    $this->assertContains('SimpleTrait', $w->getTraits()->keys());
+    $this->assertContains('MyEnum', $w->getEnums()->keys());
+    $this->assertContains('MyType', $w->getTypes()->keys());
+    $this->assertContains('MyNewtype', $w->getNewtypes()->keys());
+    $this->assertContains('generic_function', $w->getFunctions()->keys());
+    $this->assertContains('MY_CONST', $w->getConstants()->keys());
+  }
+
+  public function testPathFilters(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertContains('SingleNamespace\SimpleClass', $w->getClasses()->keys());
+
+    $w = $this->createWalker($path ==> strpos($path, 'single_namespace') === false);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertNotContains('SingleNamespace\SimpleClass', $w->getClasses()->keys());
+  }
+
+  public function testContainsDuplicates(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    
+    $files = $w->getClasses()['SimpleClass'];
+    $data = realpath(__DIR__.'/data');
+    $this->assertContains($data.'/no_namespace_php.php', $files);
+    $this->assertContains($data.'/mixed_php_html.php', $files);
+  }
+}

hhvm/definition-finder

src/TreeWalker.php
@@ -1,0 +1,91 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace Facebook\DefinitionFinder;
+
+class TreeWalker implements TreeDefinitions {
+  private Map<string, Set<string>> $classes = Map { };
+  private Map<string, Set<string>> $interfaces = Map { };
+  private Map<string, Set<string>> $traits = Map { };
+  private Map<string, Set<string>> $enums = Map { };
+  private Map<string, Set<string>> $types = Map { };
+  private Map<string, Set<string>> $newtypes = Map { };
+  private Map<string, Set<string>> $functions = Map { };
+  private Map<string, Set<string>> $constants = Map { };
+
+  /**
+   * Create a TreeWalker
+   *
+   * @param $root - the root directory
+   * @param $path_filter_func - called for every file name before it is parsed.
+   *   If it returns false, the file is not parsed. Handy if there's something
+   *   in vendor/ or your tests that does not parse.
+   */
+  public function __construct(
+    string $root,
+    ?(function(string): bool) $path_filter_func = null,
+  ) {
+    $dit = new \RecursiveDirectoryIterator($root) /* HH_FIXME[2049] no HHI */;
+    $rit = new \RecursiveIteratorIterator($dit) /* HH_FIXME[2049] no HHI */;
+    $files = Vector { };
+    foreach ($rit as $path => $info) {
+      if ($info->isDir() || $info->isLink() || !$info->isReadable()) {
+        continue;
+      }
+
+      $ext = $info->getExtension();
+      if ($ext !== 'php' && $ext !== 'hh') {
+        continue;
+      }
+
+      if ($path_filter_func !== null) {
+        if (!$path_filter_func($path)) {
+          continue;
+        }
+      }
+
+      $files[] = $path;
+    }
+    foreach ($files as $file) {
+      $fp = FileParser::FromFile($file);
+      self::addDefs($this->classes, $file, $fp->getClasses());
+      self::addDefs($this->interfaces, $file, $fp->getInterfaces());
+      self::addDefs($this->traits, $file, $fp->getTraits());
+      self::addDefs($this->enums, $file, $fp->getEnums());
+      self::addDefs($this->types, $file, $fp->getTypes());
+      self::addDefs($this->newtypes, $file, $fp->getNewtypes());
+      self::addDefs($this->functions, $file, $fp->getFunctions());
+      self::addDefs($this->constants, $file, $fp->getConstants());
+    }
+  }
+
+  private static function addDefs(
+    Map<string, Set<string>> $container,
+    string $file,
+    \ConstVector<string> $defs,
+  ): void {
+    foreach ($defs as $def) {
+      if (!$container->containsKey($def)) {
+        $container[$def] = Set { };
+      }
+      $container[$def]->add($file);
+    }
+  }
+
+  public function getClasses(): \ConstMap<string, Set<string>> { return $this->classes; }
+  public function getInterfaces(): \ConstMap<string, Set<string>> { return $this->interfaces; }
+  public function getTraits(): \ConstMap<string, Set<string>> { return $this->traits; }
+  public function getEnums(): \ConstMap<string, Set<string>> { return $this->enums; }
+  public function getTypes(): \ConstMap<string, Set<string>> { return $this->types; }
+  public function getNewtypes(): \ConstMap<string, Set<string>> { return $this->newtypes; }
+  public function getFunctions(): \ConstMap<string, Set<string>> { return $this->functions; }
+  public function getConstants(): \ConstMap<string, Set<string>> { return $this->constants; }
+}

tests/TreeWalkerTest.php
@@ -1,0 +1,73 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+use \Facebook\DefinitionFinder\TreeWalker;
+
+final class TreeWalkerTest extends PHPUnit_Framework_TestCase {
+  private function createWalker(
+    ?(function(string):bool) $filter
+  ): TreeWalker {
+    return new TreeWalker(realpath(__DIR__.'/../'), $filter);
+  }
+
+  public function testHasThisLibraryDefinitions(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains(
+      'Facebook\DefinitionFinder\TreeWalker',
+      $w->getClasses()->keys(),
+    );
+    $this->assertContains(
+      'Facebook\DefinitionFinder\DefinitionToken',
+      $w->getEnums()->keys(),
+    );
+    $this->assertContains(
+      'Facebook\DefinitionFinder\TreeDefinitions',
+      $w->getInterfaces()->keys(),
+    );
+
+    $this->assertContains(
+      realpath(__DIR__.'/../src/TreeWalker.php'),
+      $w->getClasses()['Facebook\DefinitionFinder\TreeWalker'],
+    );
+  }
+
+  public function testEachDefinitionKind(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertContains('SimpleInterface', $w->getInterfaces()->keys());
+    $this->assertContains('SimpleTrait', $w->getTraits()->keys());
+    $this->assertContains('MyEnum', $w->getEnums()->keys());
+    $this->assertContains('MyType', $w->getTypes()->keys());
+    $this->assertContains('MyNewtype', $w->getNewtypes()->keys());
+    $this->assertContains('generic_function', $w->getFunctions()->keys());
+    $this->assertContains('MY_CONST', $w->getConstants()->keys());
+  }
+
+  public function testPathFilters(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertContains('SingleNamespace\SimpleClass', $w->getClasses()->keys());
+
+    $w = $this->createWalker($path ==> strpos($path, 'single_namespace') === false);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    $this->assertNotContains('SingleNamespace\SimpleClass', $w->getClasses()->keys());
+  }
+
+  public function testContainsDuplicates(): void {
+    $w = $this->createWalker(null);
+    $this->assertContains('SimpleClass', $w->getClasses()->keys());
+    
+    $files = $w->getClasses()['SimpleClass'];
+    $data = realpath(__DIR__.'/data');
+    $this->assertContains($data.'/no_namespace_php.php', $files);
+    $this->assertContains($data.'/mixed_php_html.php', $files);
+  }
+}