kilahm/definitions-finder

Expose namespace and short name as explicit entities
New tests

[joelm@devbig429:~/local/joel-definitions-finder]  (namespace-short-name) $ hoss vendor/bin/phpunit tests/ClassContentsTest.php
PHPUnit 5.1.0 by Sebastian Bergmann and contributors.

.................                                                 17 / 17 (100%)

Time: 4.93 seconds, Memory: 9.90Mb

OK (17 tests, 54 assertions)

tests/ClassContentsTest.php
@@ -19,6 +19,33 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
     );
   }
 
+  public function testNamespaceName(): void {
+    $this->assertEquals(
+      'FredEmmott\DefinitionFinder\Test',
+      $this->class?->getNamespaceName(),
+    );
+    $this->assertEquals(
+      Vector {'', '', '', ''},
+      $this->class?->getMethods()?->map($x ==> $x->getNamespaceName()),
+    );
+  }
+
+  public function testShortName(): void {
+    $this->assertEquals(
+      'ClassWithContents',
+      $this->class?->getShortName(),
+    );
+    $this->assertEquals(
+      Vector {
+        'publicMethod',
+        'protectedMethod',
+        'privateMethod',
+        'PublicStaticMethod',
+      },
+      $this->class?->getMethods()?->map($x ==> $x->getShortName()),
+    );
+  }
+
   public function testMethodNames(): void {
     $this->assertEquals(
       Vector {
@@ -102,7 +129,7 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
       $funcs->map($x ==> $x->isPublic()),
     );
   }
-  
+
   public function testMethodsAreStatic(): void {
     $this->assertEquals(
       Vector { false, false, false, true },
@@ -186,7 +213,7 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
       $props->map($x ==> $x->getTypehint()?->getTypeName()),
     );
 
-  
+
     $this->assertEquals(
       Vector { true },
       $props->map($x ==> $x->isStatic()),

hhvm/definition-finder

tests/ClassContentsTest.php
@@ -28,6 +28,33 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
     );
   }
 
+  public function testNamespaceName(): void {
+    $this->assertEquals(
+      'Facebook\DefinitionFinder\Test',
+      $this->class?->getNamespaceName(),
+    );
+    $this->assertEquals(
+      Vector {'', '', '', ''},
+      $this->class?->getMethods()?->map($x ==> $x->getNamespaceName()),
+    );
+  }
+
+  public function testShortName(): void {
+    $this->assertEquals(
+      'ClassWithContents',
+      $this->class?->getShortName(),
+    );
+    $this->assertEquals(
+      Vector {
+        'publicMethod',
+        'protectedMethod',
+        'privateMethod',
+        'PublicStaticMethod',
+      },
+      $this->class?->getMethods()?->map($x ==> $x->getShortName()),
+    );
+  }
+
   public function testMethodNames(): void {
     $this->assertEquals(
       Vector {
@@ -111,7 +138,7 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
       $funcs->map($x ==> $x->isPublic()),
     );
   }
-  
+
   public function testMethodsAreStatic(): void {
     $this->assertEquals(
       Vector { false, false, false, true },
@@ -195,7 +222,7 @@ class ClassContentsTest extends \PHPUnit_Framework_TestCase {
       $props->map($x ==> $x->getTypehint()?->getTypeName()),
     );
 
-  
+
     $this->assertEquals(
       Vector { true },
       $props->map($x ==> $x->isStatic()),