kilahm/definitions-finder

Merge ParametersTest and ParameterVariantsTest

tests/ParametersTest.php
@@ -5,11 +5,12 @@ namespace FredEmmott\DefinitionFinder\Test;
 use FredEmmott\DefinitionFinder\FileParser;
 use FredEmmott\DefinitionFinder\ScannedClass;
 use FredEmmott\DefinitionFinder\ScannedMethod;
+use FredEmmott\DefinitionFinder\ScannedTypehint;
 
 class ParameterTest extends \PHPUnit_Framework_TestCase {
   public function testWithoutTypes(): void {
     $data = '<?hh function foo($bar, $baz) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -24,7 +25,7 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
 
   public function testWithSimpleType(): void {
     $data = '<?hh function foo(string $bar) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -39,7 +40,7 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
 
   public function testWithGenericType(): void {
     $data = '<?hh function foo(Vector<string> $bar) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -57,5 +58,39 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
       Vector { Vector { } },
       $typehint?->getGenericTypes()?->map($x ==> $x->getGenericTypes()),
     );
+  }
+
+  public function testTestWithDefault(): void {
+    $data = '<?hh function foo($bar = "baz") {}';
+    $parser = FileParser::FromData($data);
+    $function = $parser->getFunction('foo');
+
+    $params = $function->getParameters();
+    $this->assertEquals(
+      Vector { '$bar' },
+      $function->getParameters()->map($x ==> $x->getName()),
+    );
+    $this->assertEquals(
+      Vector { null },
+      $function->getParameters()->map($x ==> $x->getTypehint()),
+    );
+    $this->markTestIncomplete("can't currently retrieve default values");
+  }
+
+  public function testWithTypeAndDefault(): void {
+    $data = '<?hh function foo(string $bar = "baz") {}';
+    $parser = FileParser::FromData($data);
+    $function = $parser->getFunction('foo');
+
+    $params = $function->getParameters();
+    $this->assertEquals(
+      Vector { '$bar' },
+      $function->getParameters()->map($x ==> $x->getName()),
+    );
+    $this->assertEquals(
+      Vector { new ScannedTypehint('string', Vector { }) },
+      $function->getParameters()->map($x ==> $x->getTypehint()),
+    );
+    $this->markTestIncomplete("can't currently retrieve default values");
   }
 }

hhvm/definition-finder

tests/ParametersTest.php
@@ -14,11 +14,12 @@ namespace Facebook\DefinitionFinder\Test;
 use Facebook\DefinitionFinder\FileParser;
 use Facebook\DefinitionFinder\ScannedClass;
 use Facebook\DefinitionFinder\ScannedMethod;
+use Facebook\DefinitionFinder\ScannedTypehint;
 
 class ParameterTest extends \PHPUnit_Framework_TestCase {
   public function testWithoutTypes(): void {
     $data = '<?hh function foo($bar, $baz) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -33,7 +34,7 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
 
   public function testWithSimpleType(): void {
     $data = '<?hh function foo(string $bar) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -48,7 +49,7 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
 
   public function testWithGenericType(): void {
     $data = '<?hh function foo(Vector<string> $bar) {}';
-    
+
     $parser = FileParser::FromData($data);
     $function = $parser->getFunction('foo');
 
@@ -66,5 +67,39 @@ class ParameterTest extends \PHPUnit_Framework_TestCase {
       Vector { Vector { } },
       $typehint?->getGenericTypes()?->map($x ==> $x->getGenericTypes()),
     );
+  }
+
+  public function testTestWithDefault(): void {
+    $data = '<?hh function foo($bar = "baz") {}';
+    $parser = FileParser::FromData($data);
+    $function = $parser->getFunction('foo');
+
+    $params = $function->getParameters();
+    $this->assertEquals(
+      Vector { '$bar' },
+      $function->getParameters()->map($x ==> $x->getName()),
+    );
+    $this->assertEquals(
+      Vector { null },
+      $function->getParameters()->map($x ==> $x->getTypehint()),
+    );
+    $this->markTestIncomplete("can't currently retrieve default values");
+  }
+
+  public function testWithTypeAndDefault(): void {
+    $data = '<?hh function foo(string $bar = "baz") {}';
+    $parser = FileParser::FromData($data);
+    $function = $parser->getFunction('foo');
+
+    $params = $function->getParameters();
+    $this->assertEquals(
+      Vector { '$bar' },
+      $function->getParameters()->map($x ==> $x->getName()),
+    );
+    $this->assertEquals(
+      Vector { new ScannedTypehint('string', Vector { }) },
+      $function->getParameters()->map($x ==> $x->getTypehint()),
+    );
+    $this->markTestIncomplete("can't currently retrieve default values");
   }
 }