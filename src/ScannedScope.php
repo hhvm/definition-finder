kilahm/definitions-finder

Split out all of FileParser to separate pieces of code
This allows the parser to invoke itself, allowing nested definitions. This
is being used for namespaces, eg:

Previously: everything is in a 'file'
Now: files contain scopes. scope may contain namespaces. namespaces contain a
scope, which may contain other stuff.

src/ScannedScope.php
@@ -1,0 +1,171 @@
+<?hh // strict
+
+namespace FredEmmott\DefinitionFinder;
+
+class ScannedScope extends ScannedBase {
+
+  public function __construct(
+    private SourcePosition $position,
+    private \ConstVector<ScannedBasicClass> $classes,
+    private \ConstVector<ScannedInterface> $interfaces,
+    private \ConstVector<ScannedTrait> $traits,
+    private \ConstVector<ScannedFunction> $functions,
+    private \ConstVector<ScannedConstant> $constants,
+    private \ConstVector<ScannedEnum> $enums,
+    private \ConstVector<ScannedType> $types,
+    private \ConstVector<ScannedNewtype> $newtypes,
+  ) {
+    parent::__construct(
+      $position,
+      '__SCOPE__',
+      /* attributes = */ Map { },
+    );
+  }
+
+  public static function getType(): ?DefinitionType {
+    return null;
+  }
+
+  public function getClasses(): \ConstVector<ScannedBasicClass> {
+    return $this->classes;
+  }
+
+  public function getInterfaces(): \ConstVector<ScannedInterface> {
+    return $this->interfaces;
+  }
+
+  public function getTraits(): \ConstVector<ScannedTrait> {
+    return $this->traits;
+  }
+
+  public function getFunctions(): \ConstVector<ScannedFunction> {
+    return $this->functions;
+  }
+
+  public function getConstants(): \ConstVector<ScannedConstant> {
+    return $this->constants;
+  }
+
+  public function getEnums(): \ConstVector<ScannedEnum> {
+    return $this->enums;
+  }
+
+  public function getTypes(): \ConstVector<ScannedType> {
+    return $this->types;
+  }
+
+  public function getNewtypes(): \ConstVector<ScannedNewtype> {
+    return $this->newtypes;
+  }
+}
+
+class ScannedScopeBuilder extends ScannedSingleTypeBuilder<ScannedScope> {
+  public function __construct() {
+    parent::__construct('__SCOPE__');
+  }
+
+  private Vector<ScannedClassBuilder> $classBuilders = Vector { };
+  private Vector<ScannedFunctionBuilder> $functionBuilders = Vector { };
+  private Vector<ScannedConstantBuilder> $constantBuilders = Vector { };
+  private Vector<ScannedEnumBuilder> $enumBuilders = Vector { };
+  private Vector<ScannedTypeBuilder> $typeBuilders = Vector { };
+  private Vector<ScannedNewtypeBuilder> $newtypeBuilders = Vector { };
+
+  private Vector<ScannedNamespaceBuilder> $namespaceBuilders = Vector { };
+
+  public function addClass(ScannedClassBuilder $b): void {
+    $this->classBuilders[] = $b;
+  }
+
+  public function addFunction(ScannedFunctionBuilder $b): void {
+    $this->functionBuilders[] = $b;
+  }
+
+  public function addConstant(ScannedConstantBuilder $b): void {
+    $this->constantBuilders[] = $b;
+  }
+
+  public function addEnum(ScannedEnumBuilder $b): void {
+    $this->enumBuilders[] = $b;
+  }
+
+  public function addType(ScannedTypeBuilder $b): void {
+    $this->typeBuilders[] = $b;
+  }
+
+  public function addNewtype(ScannedNewtypeBuilder $b): void {
+    $this->newtypeBuilders[] = $b;
+  }
+
+  public function addNamespace(ScannedNamespaceBuilder $b): void {
+    $this->namespaceBuilders[] = $b;
+  }
+
+  public function build(): ScannedScope {
+    $ns = nullthrows($this->namespace);
+    $pos = nullthrows($this->position);
+
+    $classes = Vector { };
+    $interfaces= Vector { };
+    $traits = Vector { };
+    foreach ($this->classBuilders as $b) {
+      $b->setPosition($pos)->setNamespace($ns);
+      switch ($b->getType()) {
+        case ClassDefinitionType::CLASS_DEF:
+          $classes[] = $b->build(ScannedBasicClass::class);
+          break;
+        case ClassDefinitionType::INTERFACE_DEF:
+          $interfaces[] = $b->build(ScannedInterface::class);
+          break;
+        case ClassDefinitionType::TRAIT_DEF:
+          $traits[] = $b->build(ScannedTrait::class);
+          break;
+      }
+    }
+
+    $functions = $this->buildAll($this->functionBuilders);
+    $constants = $this->buildAll($this->constantBuilders);
+    $enums = $this->buildAll($this->enumBuilders);
+    $types = $this->buildAll($this->typeBuilders);
+    $newtypes = $this->buildAll($this->newtypeBuilders);
+
+    $namespaces = $this->buildAll($this->namespaceBuilders);
+    $scopes = $namespaces->map($ns ==> $ns->getContents());
+    foreach ($scopes as $scope) {
+      $classes->addAll($scope->getClasses());
+      $interfaces->addAll($scope->getInterfaces());
+      $traits->addAll($scope->getTraits());
+      $functions->addAll($scope->getFunctions());
+      $constants->addAll($scope->getConstants());
+      $enums->addAll($scope->getEnums());
+      $types->addAll($scope->getTypes());
+      $newtypes->addAll($scope->getNewtypes());
+    }
+
+    return new ScannedScope(
+      nullthrows($this->position),
+      $classes,
+      $interfaces,
+      $traits,
+      $functions,
+      $constants,
+      $enums,
+      $types,
+      $newtypes,
+    );
+  }
+
+  private function buildAll<T>(
+    \ConstVector<ScannedSingleTypeBuilder<T>> $v,
+  ): Vector<T> {
+    return $v->map($b ==> $b
+      ->setPosition(nullthrows($this->position))
+      ->setNamespace(nullthrows($this->namespace))
+      ->build()
+    )->toVector();
+  }
+
+  public function getNamespace(): string {
+    return nullthrows($this->namespace);
+  }
+}

src/ScopeConsumer.php
@@ -1,0 +1,191 @@
+<?hh // strict
+
+namespace FredEmmott\DefinitionFinder;
+
+type AttributeMap = Map<string, Vector<mixed>>;
+
+class ScopeConsumer extends Consumer {
+  protected function getEmptyBuilder(): ScannedScopeBuilder{
+    return new ScannedScopeBuilder();
+  }
+
+  public function getBuilder(): ScannedScopeBuilder {
+    $builder = (new ScannedScopeBuilder())->setNamespace('');
+    $attrs = Map { };
+
+    $tq = $this->tq;
+    $parens_depth = 0;
+    while ($tq->haveTokens()) {
+      list ($token, $ttype) = $tq->shift();
+      if ($token === '(') {
+        ++$parens_depth;
+      }
+      if ($token === ')') {
+        --$parens_depth;
+      }
+
+      if ($parens_depth !== 0 || $ttype === null) {
+        continue;
+      }
+
+      if ($ttype === T_CLOSE_TAG) { /* ?> ... <?php */
+        $this->consumeNonCode();
+      }
+
+      if ($ttype === T_SL) {
+        $attrs = $this->consumeUserAttributes();
+        continue;
+      }
+
+      if (DefinitionType::isValid($ttype)) {
+        $this->consumeDefinition(
+          $builder,
+          DefinitionType::assert($ttype),
+          $attrs,
+        );
+        $attrs = Map { };
+        continue;
+      }
+
+      // I hate you, PHP.
+      if ($ttype === T_STRING && strtolower($token) === 'define') {
+        $builder->addConstant((new DefineConsumer($tq))->getBuilder());
+        continue;
+      }
+    }
+
+    return $builder;
+  }
+
+  private function consumeDefinition(
+    ScannedScopeBuilder $builder,
+    DefinitionType $def_type,
+    AttributeMap $attrs,
+   ): void {
+    $this->consumeWhitespace();
+
+    switch ($def_type) {
+      case DefinitionType::NAMESPACE_DEF:
+        $builder->addNamespace((new NamespaceConsumer($this->tq))->getBuilder());
+        return;
+      case DefinitionType::CLASS_DEF:
+      case DefinitionType::INTERFACE_DEF:
+      case DefinitionType::TRAIT_DEF:
+        $builder->addClass(
+          (new ClassConsumer(ClassDefinitionType::assert($def_type), $this->tq))
+            ->getBuilder()
+            ->setAttributes($attrs)
+        );
+        return;
+      case DefinitionType::FUNCTION_DEF:
+        $fb = (new FunctionConsumer($this->tq))
+          ->getBuilder();
+        if ($fb) {
+          $builder->addFunction($fb->setAttributes($attrs) );
+        }
+        return;
+      case DefinitionType::CONST_DEF:
+        $builder->addConstant((new ConstantConsumer($this->tq))->getBuilder());
+        return;
+      case DefinitionType::TYPE_DEF:
+      case DefinitionType::NEWTYPE_DEF:
+      case DefinitionType::ENUM_DEF:
+        $this->consumeSimpleDefinition($builder, $def_type);
+        return;
+    }
+  }
+
+  private function consumeUserAttributes(): AttributeMap {
+    $attrs = Map { };
+    while (true) {
+      list($name, $_) = $this->tq->shift();
+      if (!$attrs->containsKey($name)) {
+        $attrs[$name] = Vector { };
+      }
+
+      list($t, $ttype) = $this->tq->shift();
+      if ($ttype === T_SR) { // this was the last attribute
+        return $attrs;
+      }
+      if ($t === ',') { // there's another
+        continue;
+      }
+
+      // this attribute has values
+      invariant(
+        $t === '(',
+        'Expected attribute name to be followed by >>, (, or ,',
+      );
+
+      while (true) {
+        list($value, $ttype) = $this->tq->shift();
+        switch ((int) $ttype) {
+          case T_CONSTANT_ENCAPSED_STRING:
+            $value = substr($value, 1, -1);
+            break;
+          case T_LNUMBER:
+            $value = (int) $value;
+            break;
+          default:
+            invariant_violation(
+              "Invalid attribute value token type: %d",
+              $ttype
+            );
+        }
+        $attrs[$name][] = $value;
+        list($t, $_) = $this->tq->shift();
+        if ($t === ')') {
+          break;
+        }
+        invariant($t === ',', 'Expected attribute value to be followed by , or )');
+      }
+      list($t, $ttype) = $this->tq->shift();
+      if ($ttype === T_SR) {
+        return $attrs;
+      }
+      invariant(
+        $t === ',',
+        'Expected attribute value list to be followed by >> or ,',
+      );
+    }
+  }
+
+  private function consumeSimpleDefinition(
+    ScannedScopeBuilder $builder,
+    DefinitionType $def_type,
+  ): void {
+    list($name, $ttype) = $this->tq->shift();
+    invariant(
+      $ttype=== T_STRING,
+      'Expected a string for %s, got %d',
+      token_name($def_type),
+      $ttype,
+    );
+    switch ($def_type) {
+      case DefinitionType::TYPE_DEF:
+        $builder->addType(new ScannedTypeBuilder($name));
+        break;
+      case DefinitionType::NEWTYPE_DEF:
+        $builder->addNewtype(new ScannedNewtypeBuilder($name));
+        break;
+      case DefinitionType::ENUM_DEF:
+        $builder->addEnum(new ScannedEnumBuilder($name));
+        $this->skipToBlock();
+        $this->consumeBlock();
+        return;
+      default:
+        invariant_violation(
+          '%d is not a simple definition',
+          $def_type,
+        );
+    }
+    $this->consumeStatement();
+  }
+
+  /** ?> ... <?php */
+  private function consumeNonCode(): void {
+    do {
+      list ($_, $ttype) = $this->tq->shift();
+    } while ($this->tq->haveTokens() && $ttype !== T_OPEN_TAG);
+  }
+}

hhvm/definition-finder

src/ScannedScope.php
@@ -1,0 +1,180 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace Facebook\DefinitionFinder;
+
+class ScannedScope extends ScannedBase {
+
+  public function __construct(
+    private SourcePosition $position,
+    private \ConstVector<ScannedBasicClass> $classes,
+    private \ConstVector<ScannedInterface> $interfaces,
+    private \ConstVector<ScannedTrait> $traits,
+    private \ConstVector<ScannedFunction> $functions,
+    private \ConstVector<ScannedConstant> $constants,
+    private \ConstVector<ScannedEnum> $enums,
+    private \ConstVector<ScannedType> $types,
+    private \ConstVector<ScannedNewtype> $newtypes,
+  ) {
+    parent::__construct(
+      $position,
+      '__SCOPE__',
+      /* attributes = */ Map { },
+    );
+  }
+
+  public static function getType(): ?DefinitionType {
+    return null;
+  }
+
+  public function getClasses(): \ConstVector<ScannedBasicClass> {
+    return $this->classes;
+  }
+
+  public function getInterfaces(): \ConstVector<ScannedInterface> {
+    return $this->interfaces;
+  }
+
+  public function getTraits(): \ConstVector<ScannedTrait> {
+    return $this->traits;
+  }
+
+  public function getFunctions(): \ConstVector<ScannedFunction> {
+    return $this->functions;
+  }
+
+  public function getConstants(): \ConstVector<ScannedConstant> {
+    return $this->constants;
+  }
+
+  public function getEnums(): \ConstVector<ScannedEnum> {
+    return $this->enums;
+  }
+
+  public function getTypes(): \ConstVector<ScannedType> {
+    return $this->types;
+  }
+
+  public function getNewtypes(): \ConstVector<ScannedNewtype> {
+    return $this->newtypes;
+  }
+}
+
+class ScannedScopeBuilder extends ScannedSingleTypeBuilder<ScannedScope> {
+  public function __construct() {
+    parent::__construct('__SCOPE__');
+  }
+
+  private Vector<ScannedClassBuilder> $classBuilders = Vector { };
+  private Vector<ScannedFunctionBuilder> $functionBuilders = Vector { };
+  private Vector<ScannedConstantBuilder> $constantBuilders = Vector { };
+  private Vector<ScannedEnumBuilder> $enumBuilders = Vector { };
+  private Vector<ScannedTypeBuilder> $typeBuilders = Vector { };
+  private Vector<ScannedNewtypeBuilder> $newtypeBuilders = Vector { };
+
+  private Vector<ScannedNamespaceBuilder> $namespaceBuilders = Vector { };
+
+  public function addClass(ScannedClassBuilder $b): void {
+    $this->classBuilders[] = $b;
+  }
+
+  public function addFunction(ScannedFunctionBuilder $b): void {
+    $this->functionBuilders[] = $b;
+  }
+
+  public function addConstant(ScannedConstantBuilder $b): void {
+    $this->constantBuilders[] = $b;
+  }
+
+  public function addEnum(ScannedEnumBuilder $b): void {
+    $this->enumBuilders[] = $b;
+  }
+
+  public function addType(ScannedTypeBuilder $b): void {
+    $this->typeBuilders[] = $b;
+  }
+
+  public function addNewtype(ScannedNewtypeBuilder $b): void {
+    $this->newtypeBuilders[] = $b;
+  }
+
+  public function addNamespace(ScannedNamespaceBuilder $b): void {
+    $this->namespaceBuilders[] = $b;
+  }
+
+  public function build(): ScannedScope {
+    $ns = nullthrows($this->namespace);
+    $pos = nullthrows($this->position);
+
+    $classes = Vector { };
+    $interfaces= Vector { };
+    $traits = Vector { };
+    foreach ($this->classBuilders as $b) {
+      $b->setPosition($pos)->setNamespace($ns);
+      switch ($b->getType()) {
+        case ClassDefinitionType::CLASS_DEF:
+          $classes[] = $b->build(ScannedBasicClass::class);
+          break;
+        case ClassDefinitionType::INTERFACE_DEF:
+          $interfaces[] = $b->build(ScannedInterface::class);
+          break;
+        case ClassDefinitionType::TRAIT_DEF:
+          $traits[] = $b->build(ScannedTrait::class);
+          break;
+      }
+    }
+
+    $functions = $this->buildAll($this->functionBuilders);
+    $constants = $this->buildAll($this->constantBuilders);
+    $enums = $this->buildAll($this->enumBuilders);
+    $types = $this->buildAll($this->typeBuilders);
+    $newtypes = $this->buildAll($this->newtypeBuilders);
+
+    $namespaces = $this->buildAll($this->namespaceBuilders);
+    $scopes = $namespaces->map($ns ==> $ns->getContents());
+    foreach ($scopes as $scope) {
+      $classes->addAll($scope->getClasses());
+      $interfaces->addAll($scope->getInterfaces());
+      $traits->addAll($scope->getTraits());
+      $functions->addAll($scope->getFunctions());
+      $constants->addAll($scope->getConstants());
+      $enums->addAll($scope->getEnums());
+      $types->addAll($scope->getTypes());
+      $newtypes->addAll($scope->getNewtypes());
+    }
+
+    return new ScannedScope(
+      nullthrows($this->position),
+      $classes,
+      $interfaces,
+      $traits,
+      $functions,
+      $constants,
+      $enums,
+      $types,
+      $newtypes,
+    );
+  }
+
+  private function buildAll<T>(
+    \ConstVector<ScannedSingleTypeBuilder<T>> $v,
+  ): Vector<T> {
+    return $v->map($b ==> $b
+      ->setPosition(nullthrows($this->position))
+      ->setNamespace(nullthrows($this->namespace))
+      ->build()
+    )->toVector();
+  }
+
+  public function getNamespace(): string {
+    return nullthrows($this->namespace);
+  }
+}

src/ScopeConsumer.php
@@ -1,0 +1,200 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace Facebook\DefinitionFinder;
+
+type AttributeMap = Map<string, Vector<mixed>>;
+
+class ScopeConsumer extends Consumer {
+  protected function getEmptyBuilder(): ScannedScopeBuilder{
+    return new ScannedScopeBuilder();
+  }
+
+  public function getBuilder(): ScannedScopeBuilder {
+    $builder = (new ScannedScopeBuilder())->setNamespace('');
+    $attrs = Map { };
+
+    $tq = $this->tq;
+    $parens_depth = 0;
+    while ($tq->haveTokens()) {
+      list ($token, $ttype) = $tq->shift();
+      if ($token === '(') {
+        ++$parens_depth;
+      }
+      if ($token === ')') {
+        --$parens_depth;
+      }
+
+      if ($parens_depth !== 0 || $ttype === null) {
+        continue;
+      }
+
+      if ($ttype === T_CLOSE_TAG) { /* ?> ... <?php */
+        $this->consumeNonCode();
+      }
+
+      if ($ttype === T_SL) {
+        $attrs = $this->consumeUserAttributes();
+        continue;
+      }
+
+      if (DefinitionType::isValid($ttype)) {
+        $this->consumeDefinition(
+          $builder,
+          DefinitionType::assert($ttype),
+          $attrs,
+        );
+        $attrs = Map { };
+        continue;
+      }
+
+      // I hate you, PHP.
+      if ($ttype === T_STRING && strtolower($token) === 'define') {
+        $builder->addConstant((new DefineConsumer($tq))->getBuilder());
+        continue;
+      }
+    }
+
+    return $builder;
+  }
+
+  private function consumeDefinition(
+    ScannedScopeBuilder $builder,
+    DefinitionType $def_type,
+    AttributeMap $attrs,
+   ): void {
+    $this->consumeWhitespace();
+
+    switch ($def_type) {
+      case DefinitionType::NAMESPACE_DEF:
+        $builder->addNamespace((new NamespaceConsumer($this->tq))->getBuilder());
+        return;
+      case DefinitionType::CLASS_DEF:
+      case DefinitionType::INTERFACE_DEF:
+      case DefinitionType::TRAIT_DEF:
+        $builder->addClass(
+          (new ClassConsumer(ClassDefinitionType::assert($def_type), $this->tq))
+            ->getBuilder()
+            ->setAttributes($attrs)
+        );
+        return;
+      case DefinitionType::FUNCTION_DEF:
+        $fb = (new FunctionConsumer($this->tq))
+          ->getBuilder();
+        if ($fb) {
+          $builder->addFunction($fb->setAttributes($attrs) );
+        }
+        return;
+      case DefinitionType::CONST_DEF:
+        $builder->addConstant((new ConstantConsumer($this->tq))->getBuilder());
+        return;
+      case DefinitionType::TYPE_DEF:
+      case DefinitionType::NEWTYPE_DEF:
+      case DefinitionType::ENUM_DEF:
+        $this->consumeSimpleDefinition($builder, $def_type);
+        return;
+    }
+  }
+
+  private function consumeUserAttributes(): AttributeMap {
+    $attrs = Map { };
+    while (true) {
+      list($name, $_) = $this->tq->shift();
+      if (!$attrs->containsKey($name)) {
+        $attrs[$name] = Vector { };
+      }
+
+      list($t, $ttype) = $this->tq->shift();
+      if ($ttype === T_SR) { // this was the last attribute
+        return $attrs;
+      }
+      if ($t === ',') { // there's another
+        continue;
+      }
+
+      // this attribute has values
+      invariant(
+        $t === '(',
+        'Expected attribute name to be followed by >>, (, or ,',
+      );
+
+      while (true) {
+        list($value, $ttype) = $this->tq->shift();
+        switch ((int) $ttype) {
+          case T_CONSTANT_ENCAPSED_STRING:
+            $value = substr($value, 1, -1);
+            break;
+          case T_LNUMBER:
+            $value = (int) $value;
+            break;
+          default:
+            invariant_violation(
+              "Invalid attribute value token type: %d",
+              $ttype
+            );
+        }
+        $attrs[$name][] = $value;
+        list($t, $_) = $this->tq->shift();
+        if ($t === ')') {
+          break;
+        }
+        invariant($t === ',', 'Expected attribute value to be followed by , or )');
+      }
+      list($t, $ttype) = $this->tq->shift();
+      if ($ttype === T_SR) {
+        return $attrs;
+      }
+      invariant(
+        $t === ',',
+        'Expected attribute value list to be followed by >> or ,',
+      );
+    }
+  }
+
+  private function consumeSimpleDefinition(
+    ScannedScopeBuilder $builder,
+    DefinitionType $def_type,
+  ): void {
+    list($name, $ttype) = $this->tq->shift();
+    invariant(
+      $ttype=== T_STRING,
+      'Expected a string for %s, got %d',
+      token_name($def_type),
+      $ttype,
+    );
+    switch ($def_type) {
+      case DefinitionType::TYPE_DEF:
+        $builder->addType(new ScannedTypeBuilder($name));
+        break;
+      case DefinitionType::NEWTYPE_DEF:
+        $builder->addNewtype(new ScannedNewtypeBuilder($name));
+        break;
+      case DefinitionType::ENUM_DEF:
+        $builder->addEnum(new ScannedEnumBuilder($name));
+        $this->skipToBlock();
+        $this->consumeBlock();
+        return;
+      default:
+        invariant_violation(
+          '%d is not a simple definition',
+          $def_type,
+        );
+    }
+    $this->consumeStatement();
+  }
+
+  /** ?> ... <?php */
+  private function consumeNonCode(): void {
+    do {
+      list ($_, $ttype) = $this->tq->shift();
+    } while ($this->tq->haveTokens() && $ttype !== T_OPEN_TAG);
+  }
+}