47ce31b82c17196a8c5093970717f831a62bff39
Fred Emmott <fe@fb.com>
2018-02-08 15:10:39 -0800

    update travis-ci config to not use docker

--- .travis.sh
+++ .travis.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 set -ex
 hhvm --version
-apt-get update -y
-apt-get install -y binutils
-echo hhvm.jit=0 >> /etc/hhvm/php.ini
-curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
-cd /var/source
-hhvm /usr/local/bin/composer install
+composer install
 
-hh_server --check $(pwd)
-hhvm -d hhvm.php7.all=0 vendor/bin/phpunit tests/
-hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
+hh_client
 
-sed -i '/enable_experimental_tc_features/d' .hhconfig
+hhvm vendor/bin/phpunit tests/
+
+echo > .hhconfig
 hh_server --check $(pwd)
