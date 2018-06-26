abad4be62ba283a1b5cf1881e14c4d0a8f67e6af
Fred Emmott <fe@fb.com>
2018-06-26 08:32:54 -0700

    Use "as" constraint as the value for abstract type constants

--- src/consumers/type_constant_from_ast.php
+++ src/consumers/type_constant_from_ast.php
@@ -16,14 +16,20 @@ function type_constant_from_ast(
   ConsumerContext $context,
   HHAST\TypeConstDeclaration $node,
 ): ScannedTypeConstant {
+  $is_abstract = $node->getAbstract() instanceof HHAST\AbstractToken;
   return (
     new ScannedTypeConstant(
       $node,
       name_from_ast($node->getName()),
       context_with_node_position($context, $node)['definitionContext'],
       /* docblock = */ null,
-      typehint_from_ast($context, $node->getTypeSpecifier()),
-      $node->getAbstract() instanceof HHAST\AbstractToken
+      typehint_from_ast(
+        $context,
+        $is_abstract
+          ? $node->getTypeConstraint()?->getType()
+          : $node->getTypeSpecifier(),
+      ),
+      $is_abstract
         ? AbstractnessToken::IS_ABSTRACT
         : AbstractnessToken::NOT_ABSTRACT,
     )
