135fc3fc179c003d0ffb2d31234965ee93b3df1b
Fred Emmott <fe@fb.com>
2018-06-28 09:40:27 -0700

    Remove unused definitions

--- src/utils.php
+++ src/utils.php
@@ -14,12 +14,3 @@ function nullthrows<T as nonnull>(?T $v): T {
   invariant($v !== null, 'unexpected null');
   return $v;
 }
-
-function normalize_xhp_class(string $in): string {
-  return 'xhp_'.\str_replace(':', '__', \substr(\strtr($in, '-', '_'), 1));
-}
-
-// Defined in runtime in global namespace, but not in HHI
-// facebook/hhvm#4872
-const int T_TYPELIST_LT = 398;
-const int T_TYPELIST_GT = 399;
